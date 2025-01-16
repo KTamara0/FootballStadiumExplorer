f4b79592a12c2e3b2b0c8e52952d775a8998a5e4
KTamara0 <tamara.kaselj00@gmail.com>
2025-01-16 20:31:55 +0100

    Football Stadium Explorer

--- app/src/main/java/com/example/footballstadiumexplorer/ui/theme/StadiumScreen.kt
+++ app/src/main/java/com/example/footballstadiumexplorer/ui/theme/StadiumScreen.kt
@@ -44,6 +44,7 @@ import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
@@ -239,7 +240,7 @@ fun StadiumCategories() {
         }
         Spacer(modifier = Modifier.width(6.dp))
         Spacer(modifier = Modifier.height(10.dp))
-        TabButton(text = "Turkey", isActive = currentActiveButton == 1) {
+        TabButton(text = "Spain", isActive = currentActiveButton == 1) {
             currentActiveButton = 1
         }
         Spacer(modifier = Modifier.width(6.dp))
@@ -343,6 +344,64 @@ val stadiums: List<Stadium> = listOf(
                 "The exterior of the stadium is designed to be in the shape of the body and head of a crocodile.",
         review = listOf(Review(user="Marko", text="Beautiful stadium!")),
         isFavorited = false
+    ),
+    Stadium(
+        image = R.drawable.acmilan,
+        name = "San Siro Stadium",
+        location = listOf(Location(city="Milan", state = "Italy")),
+        capacity = 75817,
+        year =1925,
+        description = "San Siro is home to one of the most successful European teams, Milan and city rivals Inter, " +
+                "and is one of the most famous football stadiums in the world. It was made exclusively for football, so there is no athletics track. " +
+                "The construction of the stadium began in 1925, at the initiative of the then president of AC Milan, Piero Pirelli. The first match, " +
+                "played on September 19, 1926, in which Inter defeated Milan with a score of 6:3, was attended by 35,000 spectators. In the beginning, " +
+                "only Milan played in the stadium, and since 1947 it has been shared with Inter. The Italian national football team sometimes plays matches " +
+                "at the stadium, and the finals of the Champions League and the finals of the UEFA Cup have also been played. The stadium was renovated in 1989 " +
+                "for the needs of the 1990 World Cup.",
+        review = listOf(Review(user = "Alex", text= "Amazing stadium. I am impressed.")),
+        isFavorited = false
+    ),
+    Stadium(
+        image = R.drawable.bvbstadium,
+        name = "Signal Iduna Park",
+        location = listOf(Location(city = "Dortmund", state="Germany")),
+        capacity = 81365,
+        year = 1974,
+        description = "Signal Iduna Park is a stadium in the German city of Dortmund. It was built in 1974 for the World Cup, which was held in Germany that year, and six " +
+                "matches of the 2006 World Cup were played there. The Borussia Dortmund football club plays its home games at the stadium. Signal Iduna Park has 65,718 seats " +
+                "(in international matches) and even 80,552 when standing is allowed. It is Germany's largest stadium, the sixth-largest in Europe, and the third-largest home to " +
+                "a top-flight European club after Camp Nou and Santiago Bernabéu Stadium. It holds the European record for average fan attendance, set in the 2011–12 season with " +
+                "almost 1.37 million spectators over 17 games at an average of 80,588 per game.",
+        review = listOf(Review(user = "Luka", text = "It is a wonderful experience to attend a match at a stadium like this.")),
+        isFavorited = false
+    ),
+    Stadium(
+        image = R.drawable.campnou,
+        name = "Camp Nou",
+        location = listOf(Location(city = "Barcelona", state = "Spain")),
+        capacity = 99354,
+        year = 1957,
+        description = "Camp Nou is a football stadium in Barcelona. FC Barcelona plays home matches on it. Camp Nou means new playground in Catalan. With a capacity of 99,354, " +
+                "it is now the biggest stadium in Europe. However, the total capacity has varied over the years owing to different modifications. In the 1998-99 season, UEFA " +
+                "recognised the services and facilities at Camp Nou by awarding it five star status. In 2010, in line with the new UEFA regulations, this category was replaced by " +
+                "the new 'Category 4' title which corresponds to the stadiums which fulfill the most demanding requirements with regards to facilities, services and capacity such as " +
+                "FC Barcelona's ground.",
+        review = listOf(Review(user = "Emanuel", text = "Camp Nou is an incredible experience – the atmosphere, history and size of the stadium are unrepeatable!")),
+        isFavorited = false
+    ),
+    Stadium(
+        image = R.drawable.santiagobernabeu,
+        name = "Santiago Bernabeu",
+        location = listOf(Location(city = "Madrid", state = "Spain")),
+        capacity = 78297,
+        year = 1947,
+        description = "The Santiago Bernabéu Stadium is home to the Spanish giants Real Madrid. The popular El Bernabeu, named after the former president of Real Santiago Bernabeu Yeste, " +
+                "is one of the most famous and prestigious football stadiums in Europe. It hosted the European Champions Cup final three times (1957, 1969, 1980), and the Champions League " +
+                "final for the 2009/10 season. The finals of the 1964 European Football Championship and the World Cup were also held at this stadium. In 2007, the 1000th jubilee match was " +
+                "played at the Santiago Bernabeu. UEFA declared the Santiago Bernabeu an elite European stadium after the Champions League match against Olympiacos was played on October 27. " +
+                "A month after that, the stadium celebrated the 60th anniversary of its presentation in 1947.",
+        review = listOf(Review(user = "Leo", text = "Camp Nou is an impressive stadium with a rich history and a great atmosphere.")),
+        isFavorited = false
     )
 )
 
@@ -351,11 +410,11 @@ fun StadiumCard(
     @DrawableRes imageResource: Int,
     title: String,
     subtitle: String,
-    function: () -> Unit
+    function: () -> Unit,
 ) {
     Box(
         modifier = Modifier
-            .width(200.dp)
+            .width(400.dp)
             .height(300.dp)
             .clickable(onClick = function)
             .padding(top = 8.dp, bottom = 8.dp)
@@ -374,8 +433,8 @@ fun StadiumCard(
                 .padding(horizontal = 10.dp, vertical = 10.dp),
             verticalArrangement = Arrangement.Bottom
         ) {
-            Text(title, color = Blue300)
-            Text(subtitle, color = Blue300)
+            Text(title, color = Blue300, style = TextStyle(fontWeight = FontWeight.Bold))
+            Text(subtitle, color = Blue300, style = TextStyle(fontWeight = FontWeight.Bold))
         }
     }
 }
@@ -462,14 +521,16 @@ fun ScreenInfo(
         Text(
             text = title,
             style = TextStyle(color = Color.Black, fontSize = 24.sp,
-                fontWeight = FontWeight.Bold),
+                fontWeight = FontWeight.Bold,
+                fontStyle = FontStyle.Italic
+            ),
             modifier = Modifier
                 .padding(horizontal = 16.dp)
         )
         Text(
             text = location.joinToString(", ") { "${it.city}, ${it.state}" },
-            style = TextStyle(color = LightBlue600, fontSize = 15.sp,
-                fontWeight = FontWeight.Light),
+            style = TextStyle(color = LightBlue600, fontSize = 17.sp,
+                fontWeight = FontWeight.Bold),
             modifier = Modifier
                 .padding(horizontal = 16.dp)
         )
@@ -487,7 +548,7 @@ fun InfoColumn(
         Icon(
             painter = painterResource(id = iconResource),
             contentDescription = null,
-            tint = Blue300,
+            tint = Blue400,
             modifier = Modifier.height(24.dp)
         )
         Text(text = text, fontWeight = FontWeight.Bold)
@@ -501,7 +562,7 @@ fun BasicInfo(stadium: Stadium) {
             .fillMaxWidth()
             .padding(top = 16.dp)
     ) {
-        InfoColumn(R.drawable.ic_star, stadium.capacity.toString())
+        InfoColumn(R.drawable.seat, stadium.capacity.toString())
         InfoColumn(R.drawable.ic_clock, stadium.year.toString())
     }
 }
@@ -525,11 +586,33 @@ fun Reviews(stadium: Stadium) {
         modifier = Modifier
             .fillMaxWidth()
             .padding(PaddingValues(start = 16.dp))
+            .padding(bottom = 50.dp)
     ) {
         Column {
             Text(text = "Reviews", style = TextStyle(fontSize = 16.sp,
-                fontWeight = FontWeight.Bold))
-            Text(text = stadium.review.joinToString(": ") { "${it.user}, ${it.text}" }, color = Color.DarkGray)
+                fontWeight = FontWeight.Bold,
+                fontStyle = FontStyle.Italic))
+
+            stadium.review.forEach { review ->
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.padding(vertical = 4.dp) // Razmak između recenzija
+                ) {
+                    // Korisničko ime (bold, crna boja)
+                    Text(
+                        text = "${review.user}: ",
+                        style = TextStyle(
+                            fontWeight = FontWeight.Bold,
+                            color = Color.Black
+                        )
+                    )
+                    // Tekst recenzije (siva boja)
+                    Text(
+                        text = review.text,
+                        style = TextStyle(color = Color.DarkGray)
+                    )
+                }
+            }
         }
     }
 }
@@ -554,31 +637,6 @@ fun NewReviewButton() {
 
 
 
-@Composable
-fun OtherStadiums() {
-    Row(
-        horizontalArrangement = Arrangement.SpaceBetween,
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(16.dp)
-    ) {
-        Image(
-            painter = painterResource(id = R.drawable.timsaharena),
-            contentDescription = "Timsah Arena",
-            modifier = Modifier
-                .weight(1f)
-                .clip(RoundedCornerShape(12.dp))
-        )
-        Spacer(modifier = Modifier.width(16.dp))
-        Image(
-            painter = painterResource(id = R.drawable.acmilan),
-            contentDescription = "AC Milan",
-            modifier = Modifier
-                .weight(1f)
-                .clip(RoundedCornerShape(12.dp))
-        )
-    }
-}
 
 @Composable
 fun StadiumDetailsScreen(
@@ -587,26 +645,34 @@ fun StadiumDetailsScreen(
 ) {
     val stadium = stadiums[stadiumId]
     val scrollState = rememberLazyListState()
-    LazyColumn(
-        verticalArrangement = Arrangement.Top,
-        horizontalAlignment = Alignment.Start,
-        state = scrollState,
-        modifier = Modifier
-            .fillMaxSize()
+    Box(
+        modifier = Modifier.fillMaxSize()
     ) {
-        item {
-            TopImageAndBar(
-                coverImage = stadium.image,
-                navigation = navigation,
-                stadium = stadium
-            )
-            ScreenInfo(stadium.image, stadium.name, stadium.location)
-            BasicInfo(stadium)
-            Description(stadium)
-            NewReviewButton()
-            Reviews(stadium)
-            OtherStadiums()
+        LazyColumn(
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.Start,
+            state = scrollState,
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Brush.linearGradient(colors = listOf(LightBlue100, white),
+                    start= Offset.Zero,
+                    end = Offset(0f, Float.POSITIVE_INFINITY)))
+        ) {
+            item {
+                TopImageAndBar(
+                    coverImage = stadium.image,
+                    navigation = navigation,
+                    stadium = stadium
+                )
+                ScreenInfo(stadium.image, stadium.name, stadium.location)
+                BasicInfo(stadium)
+                Description(stadium)
+                NewReviewButton()
+                Reviews(stadium)
+            }
         }
     }
 }
+
+
 
