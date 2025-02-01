f7faec28f195303abb501d079e0603983c48e949
KTamara0 <tamara.kaselj00@gmail.com>
2025-02-01 14:18:31 +0100

    Football Stadium Explorer

--- app/src/main/java/com/example/footballstadiumexplorer/ui/theme/AddReviewScreen.kt
+++ app/src/main/java/com/example/footballstadiumexplorer/ui/theme/AddReviewScreen.kt
@@ -21,7 +21,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -93,7 +92,6 @@ fun AddReviewScreen(stadiumId: Int,
                         val newReview = Review(user = userName, text = reviewText)
                         viewModel.addReviewToStadium(stadiumId, newReview)
                         navigation.popBackStack()
-                    } else {
                     }
                 },
                 modifier = Modifier.fillMaxWidth()
--- app/src/main/java/com/example/footballstadiumexplorer/ui/theme/AddStadiumScreen.kt
+++ app/src/main/java/com/example/footballstadiumexplorer/ui/theme/AddStadiumScreen.kt
@@ -1,7 +1,6 @@
 package com.example.footballstadiumexplorer.ui.theme
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -28,7 +27,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
