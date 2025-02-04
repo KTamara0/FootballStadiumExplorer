dfaa09535cfc4843b8c7af0c3f7203d4e956e65d
KTamara0 <tamara.kaselj00@gmail.com>
2025-02-04 12:55:23 +0100

    Football Stadium Explorer

--- app/src/main/java/com/example/footballstadiumexplorer/MainActivity.kt
+++ app/src/main/java/com/example/footballstadiumexplorer/MainActivity.kt
@@ -4,17 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.navigation.compose.rememberNavController
+import androidx.activity.viewModels
 import com.example.footballstadiumexplorer.data.StadiumViewModel
 import com.example.footballstadiumexplorer.ui.theme.FootballStadiumExplorerTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val stadiumViewModel: StadiumViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             FootballStadiumExplorerTheme {
-                NavigationController(viewModel = StadiumViewModel())
+                NavigationController(viewModel = stadiumViewModel)
             }
         }
     }
